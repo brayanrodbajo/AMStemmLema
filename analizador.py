29cd12fce73dcccf4b9e97956bdf039f24ba9e1b
nelson-portilla <nelson.herrera@correounvalle.edu.co>
2016-05-23 22:52:16 -0500

    Release 1.0 \n Permite las comas, tildes y espacios

--- analizador.py
+++ analizador.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 from flask import Flask, request, render_template
-#from flask import render_template
 import os, sys
 reload(sys)
 sys.setdefaultencoding('utf8')
@@ -16,7 +15,6 @@ def cargar():
 def obtener(): 
 	print "ENTRO en obtener"
 	#os.system('analyze -f /usr/local/share/freeling/config/es.cfg tagged < /var/www/html/input.txt > /var/www/html/output.txt')
-	#return 'hola mundo'
 	texto=str(request.form['id_entrada'])
 	print ("HOLA MUNDO!: ", texto)
 	analisis_morfo =	ejecutarAnal(texto)
@@ -24,59 +22,41 @@ def obtener():
 	return render_template('index.html', texto=texto, objetos=analisis_morfo)
 
 def ejecutarAnal(cadena):
-	# file=open("entrada.txt", "w")
-	# file.write(cadena)
-	# file.close()
-	listaPalabras=[]
-	listaLemas=[]
-	listaStemm=[]
-	listaEagles=[]
-	#objeto={'pal':"", 'lema':"", 'stem':"", 'eagles':""}
+	# listaPalabras=[]
+	# listaLemas=[]
+	# listaStemm=[]
+	# listaEagles=[]
 	listaObjetos=[]
 
 
 	limpio = cadena.replace('\r\n', ' ')
-	palabras = limpio.split(' ')
+	limpio = limpio.replace(',','')
+	palabras = limpio.split()
+	print palabras
 	txt_analizado =""
 	for pal in palabras:
 		listaAM=str(os.popen('echo '+pal+'| analyze -f am.cfg').read()[:-1]).split()
 		palabra=listaAM[0]
-		#objeto['pal']=palabra
 		txt_analizado += "PALABRA: "+palabra
-		listaPalabras.append(palabra)
+		#listaPalabras.append(palabra)
 		
 		lema=listaAM[1]
-		#objeto['lema']=lema
 		txt_analizado += "\n\t LEMA: "+lema
-		listaLemas.append(lema)
+		#listaLemas.append(lema)
 		
 		eagles=str(' '.join(listaAM[1:]))
 		txt_analizado += "\n\t EAGLES: "+eagles
-		listaEagles.append(eagles)
-		#objeto['eagles']=eagles
+		#listaEagles.append(eagles)		
 		
 		stemm = os.popen('php stemm_es.php "'+pal+'"').read()
-		listaStemm.append(stemm)
-		#objeto['stem']=stemm
+		#listaStemm.append(stemm)
 		txt_analizado += "\n\t STEMM: "+stemm
 		txt_analizado += '\n-----/-----\n'
-		# txt_analizado += os.popen('echo '+pal+'| analyze -f am.cfg').read()[:-1]
-		# txt_analizado += os.popen('php stemm_es.php "'+pal+'"').read()
-		# txt_analizado += '\n'
+		
 		objeto=[palabra, lema, stemm, eagles]
 		listaObjetos.append(objeto)
 
-	#return [txt_analizado, listaPalabras, listaLemas, listaEagles, listaStemm]
-	print listaObjetos
 	return listaObjetos
-
-# def leerArchivo():
-# 	file=open("salida.txt", "r")
-# 	morfo=""
-# 	for line in file:		
-# 		morfo+=line
-# 	return morfo
-
 
 if __name__ == '__main__': 
 	app.run(host='0.0.0.0', debug=True, port=12345, use_reloader=True)
