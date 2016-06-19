21f8644c3b4a9d3c2855331e12bd588306cc9034
UV <brayan.rodriguez.rivera@correounivalle.edu.co>
2016-06-19 12:48:28 -0500

    stanford

--- stanford_test.py
+++ stanford_test.py
@@ -1,31 +1,39 @@
 #Sentence Tokenize
 from nltk.tokenize import sent_tokenize
+from nltk.corpus import stopwords
 import sys
 input = ''
 with open(sys.argv[1], 'r') as my_file:
     input = my_file.read()
+    input = input.replace("."," .")
 sents = sent_tokenize(input)
+stops = set(stopwords.words('english'))
+
 print (input)
 
-#StanfordPOSTagger
+#StanfordPOSTagger converter to Bikel Format
 from nltk.tag.stanford import StanfordPOSTagger
 postagger = StanfordPOSTagger('english-bidirectional-distsim.tagger')
 # postags=[postagger.tag(sent.split()) for sent in sents]
-postags=[]
+postags_stanford=[]
+postags_bikel = "("
 for sent in sents:
-	print (postagger.tag(sent.split()))
-	postags.append(postagger.tag(sent.split()))
+	postags = postagger.tag(sent.split())
+	postags_stanford.append(postags)
+	for word in postags:
+		postags_bikel+="("+word[0]+" ("+word[1]+"))"
+
+print (postags_stanford)
+print (postags_bikel)
 
 #StanfordParser
 from nltk.parse.stanford import StanfordParser
 parser=StanfordParser(model_path="edu/stanford/nlp/models/lexparser/englishPCFG.ser.gz")
-# parsed_sents= sum([list(dep_graphs) for dep_graphs in parser.tagged_parse_sents(postags)],[])
+# parsed_sents= sum([list(dep_graphs) for dep_graphs in parser.tagged_parse_sents(postags_stanford)],[])
 # print (parsed_sents)
-
-parsed_sents= parser.tagged_parse_sents(postags)
+parsed_sents= parser.tagged_parse_sents(postags_stanford)
 # print (type(parsed_sents))
 for sent in parsed_sents:
-	# print(list(sent))
 	try:
 		for i in sent:
 			print(i)
